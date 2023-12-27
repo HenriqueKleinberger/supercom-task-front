93978a53c1e5c4a0b4ecff411390653252b89190
Henrique Kleinberger <henriquedfkl@gmail.com>
2023-12-27 21:48:00 +0200

    Home snapshot test

---
+++ src/components/Home/Home.test.tsx
@@ -0,0 +1,11 @@
+import renderer from "react-test-renderer";
+import Home from "./";
+
+jest.mock("../CardsWrapper", () => "CardsWrapper");
+jest.mock("../CardManagement", () => "CardManagement");
+
+it("changes the class when hovered", () => {
+  const component = renderer.create(<Home />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
